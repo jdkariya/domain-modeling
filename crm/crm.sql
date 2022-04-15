cc1fe3f6b0aaad70b857a5b3069ebfcdcde9e61b
jdkariya <josh.kariya@kellogg.northwestern.edu>
2022-04-15 21:25:31 +0000

    Update

--- crm/crm.sql
+++ crm/crm.sql
@@ -1,5 +1,39 @@
 -- Drops existing tables, so we start fresh with each
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS activity;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS salespeople;
 
 -- CREATE TABLES
+CREATE TABLE salespeople (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT
+);
+
+CREATE TABLE contacts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  phone_number TEXT,
+  company_id INTEGER,
+  title TEXT
+);
+
+CREATE TABLE activity (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  type TEXT,
+  date_and_time TEXT,
+  contact_id INTEGER,
+  salesperson_id INTEGER,
+  notes TEXT
+);
+
+CREATE TABLE companies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT
+);
--- tacostagram/tacostagram.sql
+++ tacostagram/tacostagram.sql
@@ -1,1 +1,41 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS comment;
+DROP TABLE IF EXISTS like;
+DROP TABLE IF EXISTS following;
 
+CREATE TABLE User (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  location TEXT
+);
+
+CREATE TABLE Post (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  image_file TEXT,
+  created_at TEXT,
+  user_id INTEGER
+);
+
+CREATE TABLE Comment (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  post_id INTEGER,
+  user_id INTEGER,
+  comment TEXT
+);
+
+CREATE TABLE Like (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  post_id INTEGER,
+  user_id INTEGER,
+  created_at TEXT
+);
+
+CREATE TABLE Following (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  follower_user_id INTEGER,
+  followed_user_id INTEGER
+);
